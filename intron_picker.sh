c132cedc5ff2c89d76901f4b32a61c606e6ded61
Alexandru Ioan Voda <alexandruioanvoda@users.noreply.github.com>
2017-03-24 11:57:12 -0700

    Update intron_picker.sh

--- intron_picker.sh
+++ intron_picker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # USAGE: ./pick_introns.sh <exons_bed or exonic_circRNA_bed> <all_introns_bed>
 
-bedtools closest -a $1 -b $2 -D a -s -io -iu | awk '$13==1' | sort -u -k4,4 | awk 'BEGIN{OFS="\t"}{print $1, $2, $3, $4, $6, $8, $9, $10, $13;}' > temp_downstream.csv
 bedtools closest -a $1 -b $2 -D a -s -io -id | awk '$13==-1' | sort -u -k4,4 | awk 'BEGIN{OFS="\t"}{print $1, $2, $3, $4, $6, $8, $9, $10, $13;}' > temp_upstream.csv
+bedtools closest -a $1 -b $2 -D a -s -io -iu | awk '$13==1' | sort -u -k4,4 | awk 'BEGIN{OFS="\t"}{print $1, $2, $3, $4, $6, $8, $9, $10, $13;}' > temp_downstream.csv
 
-cat temp_downstream.csv | awk '{print $4;}' > temp_id_downstream.csv
 cat temp_upstream.csv | awk '{print $4;}' > temp_id_upstream.csv
+cat temp_downstream.csv | awk '{print $4;}' > temp_id_downstream.csv
 
 grep -f temp_id_upstream.csv temp_id_downstream.csv > unified_ids.csv
 rm temp_id_upstream.csv temp_id_downstream.csv
