bdc28f7bd034667e22c92795161bc5a73a9ad6e6
Alexandru Ioan Voda <alexandruioanvoda@users.noreply.github.com>
2017-04-27 14:37:59 -0700

    Update intron_picker.sh

--- intron_picker.sh
+++ intron_picker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# USAGE: ./pick_introns.sh <exons_bed or exonic_circRNA_bed> <all_introns_bed>
+# USAGE: ./intron_picker.sh <exons_bed or exonic_circRNA_bed> <all_introns_bed>
+
+sortBed -i $1 > temp1
+cat temp1 > $1
+sortBed -i $2 > temp2
+cat temp2 > $2
+rm temp1 temp2
 
 bedtools closest -a $1 -b $2 -D a -s -io -id | awk '$13==-1' | sort -u -k4,4 | awk 'BEGIN{OFS="\t"}{print $1, $2, $3, $4, $6, $8, $9, $10, $13;}' > temp_upstream.csv
 bedtools closest -a $1 -b $2 -D a -s -io -iu | awk '$13==1' | sort -u -k4,4 | awk 'BEGIN{OFS="\t"}{print $1, $2, $3, $4, $6, $8, $9, $10, $13;}' > temp_downstream.csv
@@ -12,6 +18,10 @@ rm temp_id_upstream.csv temp_id_downstream.csv
 
 grep -f unified_ids.csv temp_upstream.csv | sort -k4,4 > introned_upstream.csv
 grep -f unified_ids.csv temp_downstream.csv | sort -k4,4 > introned_downstream.csv
-rm temp_upstream.csv temp_downstream.csv
+rm temp_upstream.csv temp_downstream.csv unified_ids.csv
 
-paste introned_upstream.csv introned_downstream.csv | awk 'BEGIN{OFS="\t";}{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $15, $16, $17, $18;}' > output.csv
+paste introned_upstream.csv introned_downstream.csv | awk 'BEGIN{OFS="\t";}{print $1, $2, $3, $4, $5, $6, $7, $8, $15, $16, $17}' > output.csv
+rm introned_upstream.csv introned_downstream.csv
+echo -e 'Chromosome\tStart\tEnd\tID\tStrand\tIntron_upstream_start\tIntron_upstream_end\tIntron_up_id\tIntron_down_start\tIntron_down_end\tIntron_down_id' | cat - output.csv > temp && mv temp output.csv
+tail -n +2 output.csv | awk 'BEGIN{OFS="\t";}{print $1, $6, $7, $4, "0", $5}' > upstream_introns.bed
+tail -n +2 output.csv | awk 'BEGIN{OFS="\t";}{print $1, $9, $10, $4, "0", $5}' > downstream_introns.bed
